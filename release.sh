56976720927f640abf07f7c893731760c4965902
Chris Bartlett <bartlettc@gmail.com>
2020-08-13 14:19:16 -0600

    testing release

--- release.sh
+++ release.sh
@@ -3,8 +3,6 @@
 wget --no-verbose https://github.com/saturnwire/chart-releaser/releases/download/1.1.0/chart-releaser_1.1.0_linux_amd64.tar.gz
 tar -zxvf chart-releaser_1.1.0_linux_amd64.tar.gz
 
-./cr index -h
+./cr index -o ${GITHUB_OWNER} -r ${GITHUB_REPO} -c https://charts.saturnwire.com -i ./index.yaml --token ${GITHUB_TOKEN}
 
-echo ${GITHUB_OWNER}
-echo ${GITHUB_REPO}
-
+cat index.yaml
