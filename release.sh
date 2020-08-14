6764b819ba9f049d69fc32de8db2ff67435f976f
Chris Bartlett <bartlettc@gmail.com>
2020-08-13 23:08:55 -0600

    Finalizing test script

--- release.sh
+++ release.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eu -o pipefail
 
+# Downloads helm chart-releaser
 wget --no-verbose https://github.com/saturnwire/chart-releaser/releases/download/1.1.0/chart-releaser_1.1.0_linux_amd64.tar.gz
 tar -zxvf chart-releaser_1.1.0_linux_amd64.tar.gz
-
 mkdir -p .cr-release-packages
+
+# Create the new index files with updated packages from repo
 ./cr index --remote -o ${GITHUB_OWNER} -r ${GITHUB_REPO} -c https://charts.saturnwire.com -i ./index.yaml --token ${GITHUB_TOKEN}
 
-cat index.yaml
-
+# Commit back to master
 git config --global user.email "automation@saturnwire.com"
 git config --global user.name "SaturnWire Automation"
-
 git add index.yaml
 git commit -m "Auto-releasing new index file"
 git push -u origin master
